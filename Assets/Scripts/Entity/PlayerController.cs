f05bdb07a0997d236d4afebbe344cc94156af547
totalpsh <66562462+totalpsh@users.noreply.github.com>
2025-07-29 19:15:25 +0900

    [feat] Player 가까운 적 찾기 구현

--- Assets/Scripts/Entity/PlayerController.cs
+++ Assets/Scripts/Entity/PlayerController.cs
@@ -25,15 +25,19 @@ public class PlayerController : BaseController
         camera = Camera.main;
     }*/
 
+    
+    private List<GameObject> _enemies; // 에너미들 모아서 담는 List
+    private GameObject _target;
+    private float maxFindRange = 10f;
+
+
     protected override void HandleAction()
     {
         // 키입력 방향키 or wasd
         // 이동 - 노멀라이즈
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        movementDirection = new Vector2(horizontal, vertical).normalized;
-
+        OnMove();
         // 장애물에 안걸리는 에너미 찾아서 그쪽 방향으로 바라보기
+        FindNearestEnemy();
         OnLook();
 
         // 공격
@@ -49,23 +53,24 @@ public class PlayerController : BaseController
 
     void OnMove()//InputValue inputValue)
     {
-        
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-
-        _rigidbody.velocity = new Vector2(horizontal, vertical) * statHandler.Speed;
-
-        Vector2 movement = new Vector2(horizontal, vertical);
+        movementDirection = new Vector2(horizontal, vertical).normalized;
         //movementDirection = inputValue.Get<Vector2>();
         //movementDirection = movementDirection.normalized;//InputValue의 벡터를 정해준다.
     }
 
     void OnLook()//InputValue inputValue)
     {
+        Vector2 targetPos = _target.transform.position;
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);//마우스의 위치 좌표를 받고
-        Vector2 worldPos = Camera.main.ScreenToWorldPoint(mousePosition);//카메라의 월드 좌표를 받아
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(targetPos);//카메라의 월드 좌표를 받아
 
         lookDirection = (worldPos - (Vector2)transform.position);//빼본다
+
+        //Debug.Log(Mathf.Abs(Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg));
+
         if (lookDirection.magnitude < 0.9f)
         {
             lookDirection = Vector2.zero;
@@ -83,5 +88,27 @@ public class PlayerController : BaseController
             return;//요건 캔버스가 그린 것에 마우스를 올린 뒤에 마우스를 누르면 리턴해버리겠다는 뜻
         }
         //isAttacking = inputValue.isPressed;//isAttacking에 마우스가 눌렸는지 보내준다.
+    }
+
+    void FindNearestEnemy()
+    {
+        _enemies = new List<GameObject>(GameObject.FindGameObjectsWithTag("Enemy"));
+        
+        float shortDis = Vector3.Distance(transform.position, _enemies[0].transform.position);
+        _target = _enemies[0];
+
+        foreach (GameObject enemy in _enemies)
+        {
+            float distance = Vector3.Distance(transform.position, enemy.transform.position);
+
+                Debug.Log(distance);
+
+            if(distance <= maxFindRange)
+            {
+                _target = enemy;
+                shortDis = distance;
+            }
+        }
+
     }
 }
