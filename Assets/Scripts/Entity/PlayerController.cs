9579903417e5f5dc5a44f05674ec4c41df965fea
totalpsh <66562462+totalpsh@users.noreply.github.com>
2025-07-30 00:22:15 +0900

    [Fix] Player적 찾기 자동 공격 로직 수정

--- Assets/Scripts/Entity/PlayerController.cs
+++ Assets/Scripts/Entity/PlayerController.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 //using UnityEngine.InputSystem;
 
 public class PlayerController : BaseController
 {
-    // 이동
+    // 미쳐버리겠다.
+    // 플레이어는 이동과 공격을 함
+    // 이동 중에는 공격 x, 이동을 멈추면 공격을 함
+    // 이동 중에 키로 flipX 조작, 멈추고 공격할 때는 적을 찾아서 회전을 함
+    // 
     // 각각 상태에 따른 애니메이션
-    // 공격 - 자동 - 가까운 적 찾기 - 장애물이 있다면 다른 적 찾기
+    // 공격 - 자동 - 일정 범위 내의 가까운 적 찾기 - 장애물이 있다면 다른 적 찾기
     // 적의 위치에 따른 flipX 세팅
     // 적이 존재 할경우 attack = true => 투사체 발사 시작
 
@@ -25,22 +30,19 @@ public class PlayerController : BaseController
         camera = Camera.main;
     }*/
 
-    
-    private List<GameObject> _enemies; // 에너미들 모아서 담는 List
+    [SerializeField] private float findRadius = 5f;
+    [SerializeField] private LayerMask enemyLayer;
     private GameObject _target;
-    private float _maxFindRange = 10f;
 
 
     protected override void HandleAction()
     {
-        
         OnMove();
         // 장애물에 안걸리는 에너미 찾아서 그쪽 방향으로 바라보기
-        //FindNearestEnemy();
+        FindNearestEnemy();
         OnLook();
-
         // 공격
-        isAttacking = Input.GetMouseButton(0);
+        //isAttacking = Input.GetMouseButton(0);
 
         //if(isAttacking) Attack();
     }
@@ -58,19 +60,28 @@ public class PlayerController : BaseController
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         movementDirection = new Vector2(horizontal, vertical).normalized;
+
+        // 멈춰 있을 때 공격한다.
+        if (movementDirection.x == 0 && movementDirection.y == 0)
+        {
+            isAttacking = _target != null ? true : false;
+        }
+        else isAttacking = false;
+        
         //movementDirection = inputValue.Get<Vector2>();
         //movementDirection = movementDirection.normalized;//InputValue의 벡터를 정해준다.
     }
 
     void OnLook()//InputValue inputValue)
     {
-        //Vector2 targetPos = _target.transform.position;
-        Vector2 mousePosition = Input.mousePosition;//마우스의 위치 좌표를 받고
-        Vector2 worldPos = Camera.main.ScreenToWorldPoint(mousePosition);//카메라의 월드 좌표를 받아
+        if (!isAttacking) return;
+        if (!_target ) return;
+
+        Vector2 targetPos = _target.transform.position;
+        //Vector2 mousePosition = Input.mousePosition;//마우스의 위치 좌표를 받고
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(targetPos);//카메라의 월드 좌표를 받아
 
         lookDirection = (worldPos - (Vector2)transform.position);//빼본다
-
-        Debug.Log(lookDirection.normalized);
 
         if (lookDirection.magnitude < 0.9f)
         {
@@ -93,28 +104,34 @@ public class PlayerController : BaseController
 
     void FindNearestEnemy()
     {
-        _enemies = new List<GameObject>(GameObject.FindGameObjectsWithTag("Enemy"));
-        
-        float shortDis = Vector3.Distance(transform.position, _enemies[0].transform.position);
-        _target = _enemies[0];
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, findRadius, enemyLayer);
 
-        foreach (GameObject enemy in _enemies)
+        if (colliders.Length == 0)
         {
-            float distance = Vector3.Distance(transform.position, enemy.transform.position);
-
-                Debug.Log(distance);
-
-            if(distance <= _maxFindRange)
-            {
-                _target = enemy;
-                shortDis = distance;
-                isAttacking = true;
-            }
-            else
-            {
-                isAttacking = false;
-            }
+            _target = null;
+            return;
         }
 
+        GameObject nearest = null;
+        float minDistance = Mathf.Infinity;
+
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.gameObject.layer == gameObject.layer) continue;
+
+            float distance = Vector2.Distance(transform.position, collider.transform.position);
+
+            if (distance < minDistance && distance < findRadius)
+            {
+                minDistance = distance;
+                _target = collider.gameObject;
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, 5f);
     }
 }
