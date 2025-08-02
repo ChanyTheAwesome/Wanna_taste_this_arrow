ff3faca053325df3e98f95890ccdfb75dfc69500
totalpsh <66562462+totalpsh@users.noreply.github.com>
2025-08-02 15:07:38 +0900

    [feat] 튜토리얼 기능 구현 완

--- Assets/Scripts/Utility/TutorialUI.cs
+++ Assets/Scripts/Utility/TutorialUI.cs
@@ -5,16 +5,46 @@ using UnityEngine.UI;
 
 public class TutorialUI : MonoBehaviour
 {
-    [SerializeField] private Button spawnButton;
-    public TutorialEnemySpawner spawner;
+    [SerializeField] private Button firstButton;
+    [SerializeField] private Button secondButton;
+    [SerializeField] private Button thirdButton;
+    [SerializeField] private Image firstPage;
+    [SerializeField] private Image secondPage;
     // Start is called before the first frame update
     void Start()
     {
-        spawnButton.onClick.AddListener(CallEnemySpawn);
+        firstPage.gameObject.SetActive(false);
+        secondPage.gameObject.SetActive(false);
+        secondButton.gameObject.SetActive(false);
+        thirdButton.gameObject.SetActive(false);
+
+        firstButton.onClick.AddListener(FirstPagePrint);
+        secondButton.onClick.AddListener(SecondPagePrint);
+        thirdButton.onClick.AddListener(TutorialExit);
     }
 
-    public void CallEnemySpawn()
+    public void FirstPagePrint()
     {
-        spawner.EnemySpawn();
+        firstPage.gameObject.SetActive(true);
+        firstButton.gameObject.SetActive(false);
+        secondButton.gameObject.SetActive(true);
+    }
+
+    public void SecondPagePrint()
+    {
+        firstPage.gameObject.SetActive(false);
+        secondPage.gameObject.SetActive(true);
+        firstButton.gameObject.SetActive(false);
+        secondButton.gameObject.SetActive(false);
+        thirdButton.gameObject.SetActive(true);
+    }
+
+    public void TutorialExit()
+    {
+        firstPage.gameObject.SetActive(false);
+        secondPage.gameObject.SetActive(false);
+        firstButton.gameObject.SetActive(true);
+        secondButton.gameObject.SetActive(false);
+        thirdButton.gameObject.SetActive(false);
     }
 }
